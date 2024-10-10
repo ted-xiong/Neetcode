b22f6746280fa05aa4d2c53a629e9e34e6ed17db
Ted Xiong <tedxng2001@gmail.com>
2024-10-09 23:46:35 -0500

    Added recommended solution

--- NeetCode.playground/Pages/validAnagram.xcplaygroundpage/Contents.swift
+++ NeetCode.playground/Pages/validAnagram.xcplaygroundpage/Contents.swift
@@ -45,3 +45,71 @@ isAnagram("anagram", "nagaram")
 isAnagram("rat", "cat")
 
 // man this is bad LOL 2ms runtime
+
+// MARK: Recommend solution
+class ValidAnagram {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        
+        // looks like they check for the count first.
+        // Need to get better at catching these simple edge cases
+        if s.count != t.count {
+            return false
+        }
+        
+        // They initialize two int dictionaries here
+        var countS = [Int:Int]()
+        var countT = [Int:Int]()
+        
+        // Followed by two arrays here to store the characters.
+        // They convert the strings to an array of characters here.
+        let sChars = Array(s)
+        let tChars = Array(t)
+        
+        // Here they do a lot:
+        // 1. They go through the character arrays and obtain the ascii value
+        // 2. They type cast the ascii value to Int, then store it as the dictionary key
+        
+        // 3. Now they basically use the ascii value of the character as the key. On the left hand side, that's them using it as the key. The right hand side is them using the key to access the value.
+
+        for i in 0..<sChars.count {
+            // In other words, dict[i] = 1 + dict[i], which is effectively incrementing if it exists, and storing the key-value pair if it doesn't exist.
+            
+            // NOTE: They use defaults here to combat the optional-ness of these dictionary references.
+
+            countS[Int(sChars[i].asciiValue!)] = 1 + countS[Int(sChars[i].asciiValue!), default: 0]
+            countT[Int(tChars[i].asciiValue!)] = 1 + countT[Int(tChars[i].asciiValue!), default: 0]
+            
+            print("For letter \(sChars[i]) in first word")
+            print("For letter \(tChars[i]) in second word")
+
+            print("CountS keys and values for iteration \(i): \(Int(sChars[i].asciiValue!)), \(countS[Int(sChars[i].asciiValue!)] ?? 0)")
+            print("CountT keys and values for iteration \(i): \(Int(tChars[i].asciiValue!)), \(countT[Int(tChars[i].asciiValue!)] ?? 0)")
+            print()
+
+        }
+        
+        // Here, we only care about the key while looping through the dictionary. Note the syntax.
+        for (key, _) in countS {
+            if countS[key] != countT[key, default: 0] {
+                return false
+            }
+        }
+        return true
+    }
+    
+    // This approach is MUCH simplier and cleaner, but requires sorting and not instant look-up. Not a super-scalable solution.
+    func isAnagramByLengthAndSortCheck(_ s: String, _ t: String) -> Bool {
+        
+        // if the String lengths don't match, then they cannot be anagrams of each other
+        guard s.count == t.count else { return false }
+        
+        // sorting the String Characters puts them in the same order, which allows us to check equality
+        guard s.sorted() == t.sorted() else { return false }
+        
+        // if both guards succeed, then we have an anagram
+        return true
+    }
+}
+
+ValidAnagram().isAnagram("anagram", "nagaram")
+ValidAnagram().isAnagram("rat", "cat")
