dca058f749ce4b9ea3a7d2dc7a73ac4200f47c51
Ted Xiong <tedxng2001@gmail.com>
2024-10-16 00:25:59 -0500

    their solution

--- NeetCode.playground/Pages/encodeAndDecode.xcplaygroundpage/Contents.swift
+++ NeetCode.playground/Pages/encodeAndDecode.xcplaygroundpage/Contents.swift
@@ -25,5 +25,33 @@ func decode(_ s: String) -> [String] {
     return ans.dropLast()
 }
 
+// MARK: What the hell is this solution???
 
+func encodeSolution(_ strs: [String]) -> String {
+        if strs.isEmpty { return "#" }
+        var counts = [String]()
+        for str in strs {
+            counts.append("\(str.count)")
+        }
+        return counts.joined(separator: ",") + "#" + strs.joined()
+    }
+
+func decodeSolution(_ s: String) -> [String] {
+    if s == "#" { return [] }
+    let index = s.firstIndex(of: "#")!
+    let counts = String(s[s.startIndex...s.index(before: index)]).components(separatedBy: ",")
+    var sIndex = s.index(after: index)
+    var decodedStrings = [String]()
+    for count in counts {
+        let endIndex = s.index(sIndex, offsetBy: Int(count)! - 1)
+        if sIndex > endIndex {
+            decodedStrings.append("")
+            continue
+        }
+        decodedStrings.append(String(s[sIndex...endIndex]))
+        sIndex = s.index(after: endIndex)
+    }
+    return decodedStrings
+
+}
 
