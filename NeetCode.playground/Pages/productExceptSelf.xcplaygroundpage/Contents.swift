ba44904482eb4ba3cfb2b3c339b8a46cbf5475a7
Ted Xiong <tedxng2001@gmail.com>
2024-10-18 01:50:15 -0500

    setting up productExceptSelf

--- NeetCode.playground/Pages/productExceptSelf.xcplaygroundpage/Contents.swift
+++ NeetCode.playground/Pages/productExceptSelf.xcplaygroundpage/Contents.swift
@@ -1,7 +1,21 @@
-//: [Previous](@previous)
+// MARK: My solution
 
-import Foundation
-
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+// We want to find all the possible products of the numbers in the array excluding the number that the iteration is currently on
+// For example: [1, 2, 3, 4] should return [24, 12, 8, 6]
+func productExceptSelf(_ nums: [Int]) -> [Int] {
+    // It sounds like it's possible to solve this in constant time, which means that a for-loop isn't even neccessary.
+    
+    // The obvious method is to use division, but that's not allowed.
+    
+    var count = nums.count-1
+    
+    var start = 0
+    
+    for index in 0..<nums.count {
+        
+    }
+    
+    
+    
+    return []
+}
